6a2effccaa35e7dbe9bc7df502ef61205b7a734d
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-11-09 20:47:47 -0600

    feat: padovan susbsys

---
+++ act7/act7.srcs/sources_1/new/padovan_subsystem.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+module padovan_subsystem(
+
+    input clk,
+    input rst,
+    input en_r1,
+    input en_r2,
+    input en_r3,
+    output [15:0] result
+);
+
+wire [15:0] r1_out, r2_out, r3_out, sum_out;
+
+// P(n) = P(n-2) + P(n-3)
+// Inicialización: P(0)=1, P(1)=0, P(2)=0
+
+registro_padovan U_R1(sum_out, en_r1, clk, rst, r1_out);    // P(n)
+registro_padovan U_R2(r1_out, en_r2, clk, rst, r2_out);     // P(n-1)
+registro_padovan U_R3(r2_out, en_r3, clk, rst, r3_out);     // P(n-2)
+
+sumador_16b U_SUM(r2_out, r3_out, sum_out);
+
+assign result = r1_out;
+
+endmodule
